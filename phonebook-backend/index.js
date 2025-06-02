16d0b893866b10173e8ca7b8ee6f2394e324f6a3
serfoll <sergiofollyvi@gmail.com>
2025-06-02 16:43:36 +0200

    create express server with get request for persons

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,11 +3,40 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Phonebook backend</h1>");
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (requesst, response) => {
+  response.send("<h1>Phonebook backend api</h1>");
+});
+
+app.get("/api/persons", (requesst, response) => {
+  response.json(persons);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`App is running on ${PORT}, http://localhost:${PORT}`);
+  console.log(
+    `App is running on port: ${PORT}, with url: http://localhost:${PORT}`
+  );
 });
