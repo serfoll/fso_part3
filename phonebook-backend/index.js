bc5d9cfe99e08f6ac1344322cc9c3946cb8a8fbf
serfoll <sergiofollyvi@gmail.com>
2025-06-04 14:41:13 +0200

    added morgan for logging

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,7 +2,10 @@
 
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 app.use(express.json());
+app.use(morgan("tiny"));
 
 const helpers = require("./utils/helpers");
 
