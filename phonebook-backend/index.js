46ec03ee12c9a34400fb33c6104e8145783b414a
serfoll <sergiofollyvi@gmail.com>
2025-06-04 21:34:36 +0200

    build update with proxy

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,10 +2,8 @@
 
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 
-app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 
