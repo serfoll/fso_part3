2ee408f7a4bff4d392c0c15b4e6a11a076f5d76d
serfoll <sergiofollyvi@gmail.com>
2025-06-08 23:18:16 +0200

    linting and code formating

--- phonebook-backend/models/person.js
+++ phonebook-backend/models/person.js
@@ -1,17 +1,17 @@
 /** @format */
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to db");
+  .then(() => {
+    console.log('connected to db')
   })
-  .catch((error) => console.error("connection to db failed", error.message));
+  .catch((error) => console.error('connection to db failed', error.message))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -24,25 +24,25 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /\d{3}-\d{3}-\d{4}/.test(v);
+        return /\d{3}-\d{3}-\d{4}/.test(v)
       },
       message: (props) => {
-        const msg = `Path (${props.path}) (${props.value})`;
+        const msg = `Path (${props.path}) (${props.value})`
 
-        return props.value.split("-").join("").length < 8
+        return props.value.split('-').join('').length < 8
           ? `${msg} is shorter than the minimum allowed length (8).`
-          : `${msg} is not a valid number, must be format (111-111-1111)`;
+          : `${msg} is not a valid number, must be format (111-111-1111)`
       },
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- phonebook-backend/utils/helpers.js
+++ phonebook-backend/utils/helpers.js
@@ -1,11 +1,11 @@
 /** @format */
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 const {
   Types: { ObjectId },
-} = mongoose;
+} = mongoose
 
 const isValidObjectId = (id) =>
-  ObjectId.isValid(id) && new ObjectId(id).toString() === id;
+  ObjectId.isValid(id) && new ObjectId(id).toString() === id
 
-module.exports = { isValidObjectId };
+module.exports = { isValidObjectId }
